01dec6561b8df55abe77f0cd7fdb4569c8836380
hoangpn <hoangpn314@gmail.com>
2022-06-16 12:53:17 +0700

    update readme

--- src/main/java/edu/ptit/da2020/constant/FileConstant.java
+++ src/main/java/edu/ptit/da2020/constant/FileConstant.java
@@ -2,10 +2,10 @@ package edu.ptit.da2020.constant;
 
 public class FileConstant {
 
-  public static final String MAP_FILE = "/home/hoangpn/Downloads/map";
+//  public static final String MAP_FILE = "/home/hoangpn/Downloads/map";
 //      public static final String MAP_FILE = "/home/noha/Downloads/map";
 //    public static final String MAP_FILE = "C:\\Users\\ntnhu\\Documents\\doan\\map";
-//    private static final String MAP_FILE = "src/main/resources/map/map/mapPTIT";
+    public static final String MAP_FILE = "src/main/resources/map/mapPTIT.xml";
   public static final String RAW = "src/main/resources/map/raw_node.txt";
   public static final String EDGE = "src/main/resources/map/HN_edge.txt";
   public static final String VERTEX = "src/main/resources/map/HN_vertex.txt";
